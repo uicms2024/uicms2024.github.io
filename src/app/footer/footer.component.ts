e5dd15c53a23449ee8b04c5068f01f65d9d831f8
Rizauddin Saian <rizauddin@gmail.com>
2024-02-14 10:59:08 +0800

    Add back to top arrow button

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { environment } from '../../environments/environment';
 
 @Component({
@@ -13,4 +13,15 @@ export class FooterComponent {
   confUrl = environment.confUrl;
   confEmail = environment.confEmail;
   confFB = environment.confFB;
+
+  /* Back to top arrow to scroll back to top of the page */
+  isShowBackToTopButton = false;
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.isShowBackToTopButton = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20);
+  }
+  scrollToTop(event: Event) {
+    event.preventDefault();
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
 }
